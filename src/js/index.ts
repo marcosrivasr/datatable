f2f2e9f6920ed443583d08e892f6cd82be5842ff
Marcos Rivas Rojas <marcosrivasr@gmail.com>
2020-06-23 00:36:18 -0500

    NEW: columns mapping

--- src/js/index.ts
+++ src/js/index.ts
@@ -1,4 +1,33 @@
 
-class DataTable{
-    
+interface IDataTableData{
+    _columns: string[],
+    _items:object[]
+}
+
+
+class DataTable implements IDataTableData{
+
+    _selector:string;
+    _columns: string[];
+    _items:object[];
+
+    constructor(selector: string){
+        this._selector = selector;
+        this._columns = [];
+        this._items = [];
+    }
+
+    public create(){
+        const el = document.querySelector(this._selector);
+        if(el?.tagName.toLowerCase() != 'table') throw new Error('Table is required');
+        
+        const headers = <HTMLElement[]>[].slice.call(el.querySelector('thead tr')?.children!);
+        
+        headers.forEach(element => {
+            this._columns.push(element.textContent!);
+        });
+
+        console.log(this._columns);
+        
+    }
 }
