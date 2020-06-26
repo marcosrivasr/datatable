947beee20dd1adbd8c6c3a0c641b7c6e0c477cb4
Marcos Rivas Rojas <marcosrivasr@gmail.com>
2020-06-26 11:50:38 -0500

    json properties

--- src/js/index.ts
+++ src/js/index.ts
@@ -1,25 +1,34 @@
 
-interface IDataTableData{
-    _columns: string[],
-    _items:object[]
-}
-
-
-class DataTable implements IDataTableData{
-
+class DataTable{
     _selector:string;
     _columns: string[];
     _items:object[];
+    _elementsPerPage:number;
+    _settings: object;
 
     constructor(selector: string){
         this._selector = selector;
         this._columns = [];
         this._items = [];
+        this._elementsPerPage = 5;
+        this._settings = {
+            showCheckboxes: true,
+            showHeaderButtons: 'always',
+            showSearch: true,
+            showEntries: true,
+            numberOfEntries: 5,
+            headerButtons: ['New', 'Edit', 'Remove']
+        };
     }
 
-    public createFromTable  (){
+    public createFromTable(){
+        
+        this.tokenizeTable();
+    }
+
+    private tokenizeTable():void{
         const el = document.querySelector(this._selector);
-        if(el?.tagName.toLowerCase() != 'table') throw new Error('Table is required');
+        if(el?.tagName.toLowerCase() != 'table') throw new Error('Table is required. ' + el?.tagName);
          
         const headers = <HTMLElement[]>[].slice.call(el.querySelector('thead tr')?.children!);
         const trs = <HTMLElement[]>[].slice.call(el.querySelector('tbody')?.children!);
